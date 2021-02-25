567f4dec32068eb8eeaf02329dd850b5fbadaf32
miurakazuma67 <miurakazuma67@gmail.com>
2021-02-25 17:16:50 +0900

    1回目の提出

--- Task-4/ViewController.swift
+++ Task-4/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var num: Int = 0
+    @IBOutlet private weak var label: UILabel!
+    @IBOutlet private weak var plusButton: UIButton!
+    @IBOutlet private weak var clearButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        label.text = "\(num)"
     }
 
-
+    @IBAction func plus(_ sender: Any) {
+        self.num = num + 1
+        self.label.text = "\(num)"
+    }
+    
+    @IBAction func clear(_ sender: Any) {
+        self.num = 0
+        self.label.text = "\(num)"
+    }
 }
 
